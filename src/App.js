add9391fb269192aca95afbac6fb7f33902c74da
Renato <rkauric@vvg.hr>
2022-09-24 20:30:18 +0200

    Added additional routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css'
-import {  useState} from "react";
+import { useState } from "react";
 import Main from "./Main";
 import Checkout from "./Checkout";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import OrderSummary from "./OrderSummary";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Main/>}/>
-                <Route path="/checkout" element={<Checkout/>}/>
+                <Route path="/" element={<Main />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path='/order-summary' element={<OrderSummary />} />
             </Routes>
         </Router>)
 }
